54cb7d4228a1a63146ba4ac3d81483e07aaef3a7
Mike Dacre <mike.dacre@gmail.com>
2015-03-12 12:25:45 -0700

    Added documentation to source code

--- mapping/find_intersecting_snps.py
+++ mapping/find_intersecting_snps.py
@@ -1,4 +1,28 @@
 #!/bin/env python3
+"""
+===============================================================================
+                            find_intersecting_snps
+
+Searched a previously mapped bam or sam file for mapped reads that overlap
+single nucleotide polymorphisms (SNPs). For every read that overlaps a SNP, its
+genotype is swapped with that of the other allele and the new fastq file is
+output to <input_name>.remap.fq.gz.
+This file can then be remapped to the genome and filter_remapped_reads can be
+used to retrieve properly mapped reads.
+
+Output files:
+<input>.sort.bam        - Sorted bamfile of the original input
+<input>.keep.bam        - bamfile with reads that did not intersect SNPs and
+                          therefore can be kept without remapping
+<input>.to.remap.bam    - bamfile with original reads that need to be remapped
+<input>.to.remap.num.gz - matched lines with the input.to.remap.bam that
+                          indicate the number of variants of the original read
+                          that must be remapped
+<input>.remap.fq.gz     - fastq file containing the new variants to remap.
+                          Will be .fq1.gz and .fq2.gz if the paired end option
+                          is used
+===============================================================================
+"""
 import sys, pysam, gzip, pdb, argparse, array
 #from pympler import asizeof
 
@@ -407,11 +431,18 @@ class Bam_scanner:
         return reverse
 
 def main():
-    parser=argparse.ArgumentParser()
-    parser.add_argument("-p", action='store_true', dest='is_paired_end', default=False)
-    parser.add_argument("-m", action='store', dest='max_window', type=int, default=100000)
-    parser.add_argument("infile", action='store')
-    parser.add_argument("snp_dir", action='store')
+    """ Run as a script """
+    parser = argparse.ArgumentParser(
+                 description=__doc__,
+                 formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # Arguments
+    parser.add_argument("-p", action='store_true', dest='is_paired_end',
+                        default=False, help="Paired end data (default is single)")
+    parser.add_argument("-m", action='store', dest='max_window', type=int,
+                        default=100000, help="Changes the maximum window to search for SNPs.  The default is 100,000 base pairs.  Reads or read pairs that span more than this distance (usually due to splice junctions) will be thrown out.  Increasing this window allows for longer junctions, but may increase run time and memory requirements.")
+    parser.add_argument("infile", help=".bam file from the initial mapping process")
+    parser.add_argument("snp_dir", help="Directory containing SNPs segregating within the sample in question. Should contain sorted files of SNPs separated by chromosome and named: chr<#>.snps.txt.gz with three columns: position RefAllele AltAllele")
 
     options=parser.parse_args()
     infile=options.infile
@@ -455,4 +486,3 @@ def main():
 if __name__ == '__main__':
     """Run directly"""
     main()
-
